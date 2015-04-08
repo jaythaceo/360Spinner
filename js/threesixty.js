d6f3d6c6298db174134dfd12b9b7a130ff450654
jaythaceo <jaythaceo@gmail.com>
2015-04-08 13:06:03 -0400

    [chore] Added threesixty.js

---
+++ js/threesixty.js
@@ -0,0 +1,8 @@
+/**
+ * We wrap all our code in the jQuery "DOM-ready" function to make sure the script runs only
+ * after all the DOm elements are rendered and ready to take action
+ */
+
+$(document).ready(function(){
+  /* Tells if the app is ready for user interaction */
+}):
