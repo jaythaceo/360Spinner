36405ad21b1314342db87d8046c7575cf4f5d753
jaythaceo <jaythaceo@gmail.com>
2015-04-08 23:49:48 -0400

    [chore] Refactored threesixty.js code

--- js/threesixty.js
+++ js/threesixty.js
@@ -6,39 +6,41 @@
 $(document).ready(function(){
   /* Tells if the app is ready for user interaction */
   var ready = false,
-            // Tells the app if the user is dragging the pointer.
-            dragging = false,
-            // Stores the pointer starting X position for the pointer tracking
-            pointerStartPosX = 0,
-            // Stores the pointer ending X position for the pointer tracking.
-            pointerEndPosX = 0,
-            // Stores the distance between starting and ending positions to each time period
-            pointerDistance = 0,
 
-            monitorStartTime = 0,
-            monitorInt = 10,
-            ticker = 0,
-            speedMultiplier = 10,
-            spinner,
+    // Tells the app if the user is dragging the pointer.
+    dragging = false,
+    // Stores the pointer starting X position for the pointer tracking
+    pointerStartPosX = 0,
+    // Stores the pointer ending X position for the pointer tracking*/
+    pointerEndPosX = 0,
+    // Stores the distance between starting and ending positions to each time period
+    pointerDistance = 0,
 
-            totalFrames = 180,
-            currentFrame = 0,
-            frames = [],
-            endFrame = 0,
-            loadedImages = 0,
+    monitorStartTime = 0,
+    monitorInt = 10,
+    ticker = 0,
+    speedMultiplier = 10,
+    spinner,
 
-            $document = $(document),
-            $container = $('#threesixty'),
-            $images = $('#threesixty_images'),
+    totalFrames = 180,
+    currentFrame = 0,
+    frames = [],
+    endFrame = 0,
+    loadedImages = 0,
 
-            demoMode = false,
-            fakePointer = {
-              x: 0;
-              speed: 4
-            },
-            fakePointerTimer = 0;
+    $document = $(document),
+    $container = $('#threesixty'),
+    $images = $('#threesixty_images'),
+
+    demoMode = false,
+    fakePointer = {
+      x: 0;
+      speed: 4
+     },
+    fakePointerTimer = 0;
 
   function addSpinner (){
-           
+
+
   };
 }):
