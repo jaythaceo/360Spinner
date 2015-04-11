68348e758e5d6f96436774657d3221538d1d70fd
jaythaceo <jaythaceo@gmail.com>
2015-04-11 15:21:19 -0400

    [chore] Fixed typo in comments

--- js/threesixty.js
+++ js/threesixty.js
@@ -1,6 +1,6 @@
 /**
  * We wrap all our code in the jQuery "DOM-ready" function to make sure the script runs only
- * after all the DOm elements are rendered and ready to take action
+ * after all the Dom elements are rendered and ready to take action
  */
 
 $(document).ready(function(){
